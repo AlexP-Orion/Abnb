8a0fc5e2f4845729602fe636c372ac08c2f542f4
AlexP-Orion <e0850218249@live.uleam.edu.ec>
2026-06-22 22:03:30 -0500

    ✨ Agregar soporte para PostgreSQL y actualizar referencias de paquetes en proyectos

--- Abnb.AppHost/AppHost.cs
+++ Abnb.AppHost/AppHost.cs
@@ -1,3 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var dbServer = builder.AddPostgres("dbserver").WithLifetime(ContainerLifetime.Persistant);
+dbServer.WithPgAdmin(options => 
+    options.WithHostPort(5555).WithLifetime(ContainerLifetime.Persistant)
+);
+
+var db = dbServer.AddDatabase("abnbdb");
+
+builder.AddProject<Projects.Api>("Api").WithExternalHttpEndpoints().WithReference(db).WaitFor(db);
+
 builder.Build().Run();
